ec2f61faf26aee8de641703c2023f3b990c8a1ed
mahmoodcse <93521627+mahmoodcse@users.noreply.github.com>
2021-11-16 11:05:22 +0600

    Add files via upload

--- From Input Output/javascript.js
+++ From Input Output/javascript.js
@@ -1,6 +1,154 @@
-let saveBtn = document.getElementById('save-btn');
+//add row
+let rwIndex,table = document.getElementById('info-table');
+let sId = document.getElementById('sid'),
+name = document.getElementById('name'),
+department = document.getElementById('department'),
+email = document.getElementById('email');
+
+function addHtmlTableRow(){
+  let newRow = table.insertRow(table.length),
+  cell1 = newRow.insertCell(0),
+  cell2 = newRow.insertCell(1),
+  cell3 = newRow.insertCell(2),
+  cell4 = newRow.insertCell(3);
+
+
+  cell1.innerHTML = sId.value;
+  cell2.innerHTML = name.value;
+  cell3.innerHTML = department.value;
+  cell4.innerHTML = email.value;
+selectedRowInput();
+
+
+}
+
+
+//display sellected row data into input
+function selectedRowInput(){
+
+  for(let i=1;i < table.rows.length;i++){
+    table.rows[i].onclick = function(){
+      rwIndex = this.rowIndex;
+      if(rwIndex){
+      table.rows[rwIndex].bgColor = "green";}
+      console.log(rwIndex);
+      sId.value = this.cells[0].innerHTML;
+      name.value = this.cells[1].innerHTML;
+      department.value = this.cells[2].innerHTML;
+      email.value = this.cells[3].innerHTML;
+    };
+  }
+}
+
+selectedRowInput();
+
+function editSelectedRow(){
+  table.rows[rwIndex].cells[0].innerHTML = sId.value;
+  table.rows[rwIndex].cells[1].innerHTML = name.value;
+  table.rows[rwIndex].cells[2].innerHTML = department.value;
+  table.rows[rwIndex].cells[3].innerHTML = email.value;
+}
+
+
+
+function removeSelectedRow(){
+  if(rwIndex > 0){
+  table.deleteRow(rwIndex);
+  rwIndex = null;
+}
+  else{
+    alert("Please select a row!");
+  }
+}
+
+
+//jquery block
+
+$("document").ready(function(){
+  $("#showAddform").click(function(){
+    $(".display-show").show();
+    $("#save-btn").show();
+    $("#update-btn").hide();
+  });
+
+  $("#close-btn").click(function(){
+    $(".display-show").hide();
+  });
+
+  $(".corner-close").click(function(){
+    $(".display-show").hide();
+  });
+
+  $("#showEditform").click(function(){
+    $(".display-show").show();
+    $("#update-btn").show();
+    $("#save-btn").hide();
+  });
+
+
+
+});
+/*
+let rwIndex,
+table = document.getElementById('table'),
+sId = document.getElementById('sid').value,
+name = document.getElementById('name').value,
+department = document.getElementById('department').value,
+email = document.getElementById('email').value;
+
+function addHtmlTableRow(){
+  var newRow = table.insertRow(table.length),
+  cell1 = newRow.insertCell(0),
+  cell2 = newRow.insertCell(1),
+  cell3 = newRow.insertCell(2),
+  cell4 = newRow.insertCell(3);
+
+  cell1.innerHTML = sId;
+  cell2.innerHTML = name;
+  cell3.innerHTML = department;
+  cell4.innerHTML = email;
+
+  selectedRowInput();
+}
+
+
+//display sellected row data into input
+function selectedRowInput(){
+
+  for(var i=0;i < table.rows.length;i++){
+    table.rows[i].onclick = function(){
+      rwIndex = this.rowIndex;
+      console.log(rwIndex);
+      sId = this.cells[0].innerHTML;
+      name = this.cells[1].innerHTML;
+      department = this.cells[2].innerHTML;
+      email = this.cells[3].innerHTML;
+    };
+  }
+}
+
+selectedRowInput();
+
+function editSelectedRow(){
+  table.rows[rwIndex].cells[0].innerHTML = sId;
+  table.rows[rwIndex].cells[1].innerHTML = name;
+  table.rows[rwIndex].cells[2].innerHTML = department;
+  table.rows[rwIndex].cells[3].innerHTML = email;
+}
+
+
+
+function removeSelectedRow(){
+  table.deleteRow(rwIndex);
+}
+*/
+
+
+/*let saveBtn = document.getElementById('save-btn');
+let editBtn = document.getElementById('edit-btn');
 
 let output = document.querySelector('tbody');
+let infoTable = document.querySelector('table');
 
 let sId = document.getElementById('s_id');
 let sName = document.getElementById('sname');
@@ -10,11 +158,24 @@ let sDistrict = document.getElementById('sdistrict');
 
 let profiles = [];
 let x = new Array();
+let i;
 
 
 
 
+/*  for( i=0; i < output.rows.length; i++){
+    output.rows[i].onclick = function(){
+      rwIndex = this.rowIndex;
+      sId.value = this.cells[0].innerHTML;
+      sName.value = this.cells[1].innerHTML;
+      sEmail.value = this.cells[2].innerHTML;
+      sDepartment.value = this.cells[3].innerHTML;
+      sDistrict.value = this.cells[4].innerHTML;
+      console.log(rwIndex);
+  };
+}
 
+//save button
 saveBtn.onclick = function(){
   const person ={
     id: sId.value,
@@ -32,15 +193,16 @@ sEmail.value  = "";
 sDepartment.value = "";
 sDistrict.value = "";
   showdata();
-
 };
+
+//edit button
+
+
 
 function showdata(){
 output.innerHTML = '';
-
 x = JSON.parse(localStorage.getItem("p"));
 x.forEach(data => {
-
   output.innerHTML += `
   <tr>
    <td>${data.id}</td>
@@ -55,6 +217,12 @@ x.forEach(data => {
 
 
 $(".add-btn").click(function(){
+
+  $(".display-show").show();
+
+});
+
+$(".edit-btn").click(function(){
 
   $(".display-show").show();
 
@@ -75,16 +243,7 @@ $("#cancel-btn").click(function(){
 
 
 
-
-
-
-
-
-
-
-
-
-
+*/
 
 
 
